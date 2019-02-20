e616605791f8fd8f4a21be89f05c9c45ea7e7ee6
Pete Kreppein <pmkreppein@gmail.com>
2019-02-20 12:43:51 -0500

    Routing change for root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get 'forecast/:latlong' => 'forecast#index', :constraints  => { :latlong => /.*/ }
+  root :to => "forecast#index", :latlong => '40.7128,-74.0060'
 end
